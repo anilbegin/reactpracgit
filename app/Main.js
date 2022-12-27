cef31c06a9ace0d36c36b4e40b50207e324ca153
anilbegin <98578895+anilbegin@users.noreply.github.com>
2022-12-27 19:49:11 +0530

    introduced React useState in LikeArea component

--- app/Main.js
+++ app/Main.js
@@ -1,5 +1,5 @@
 //import './main.css'
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom/client'
 
 function OurApp() {
@@ -16,11 +16,26 @@ function OurHeader() {
     return <h2 className='special'>Our Amazing App</h2>
 }
 function LikeArea() {
+    const [likeCount, setLikeCount] = useState(0)
+
+    function increaseLikes() {
+        setLikeCount(prev => prev + 1)
+    }
+
+    function decreaseLikes(){
+        setLikeCount((prev) => {
+            if(prev > 0) {
+                return prev -1
+            }
+            return 0
+        })
+    }
+
     return (
         <>
-            <h3>The page has been liked 0 times.</h3>
-            <button>Increase Likes</button>
-            <button>Decrease Likes</button>
+            <h3>The page has been liked {likeCount} times.</h3>
+            <button onClick={increaseLikes}>Increase Likes</button>
+            <button onClick={decreaseLikes}>Decrease Likes</button>
         </>
     )
 }
