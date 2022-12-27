3b48c595a72f316f25cd5504f4bfd71624343188
anilbegin <98578895+anilbegin@users.noreply.github.com>
2022-12-27 19:30:16 +0530

    added LikeArea component

--- app/Main.js
+++ app/Main.js
@@ -6,6 +6,7 @@ function OurApp() {
     return (
         <>
             <OurHeader />
+            <LikeArea />
             <TimeArea />
         </>
     )
@@ -13,6 +14,15 @@ function OurApp() {
 
 function OurHeader() {
     return <h2 className='special'>Our Amazing App</h2>
+}
+function LikeArea() {
+    return (
+        <>
+            <h3>The page has been liked 0 times.</h3>
+            <button>Increase Likes</button>
+            <button>Decrease Likes</button>
+        </>
+    )
 }
 function TimeArea() {
     return <p>The current time is {new Date().toLocaleString()}</p>
